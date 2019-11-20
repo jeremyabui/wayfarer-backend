187da234fc027c397ce523bd6a61f319e128790f
FelipeCosta <fac.10@outlook.com>
2019-11-20 14:56:36 -0800

    pulling again

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
+const db = require('./models');
 
 const PORT = process.env.PORT || 4000;
+
+const routes = require('./routes');
 // Middleware
 
 // Routes
+app.get('/', (req, res) => {
+    res.send('<h1>Project Wayfarer</h1>')
+});
+
+// Api Route
+// app.use('/api/v1', routes.api);
 
 // Server start
 app.listen(PORT, () =>
