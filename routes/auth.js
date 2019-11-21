0445e0875a484b85996cc87ef69a27e92703fa97
jbui <jeremyabui@yahoo.com>
2019-11-20 16:59:38 -0800

    Fixed login route

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const ctrl = require('../controllers')
 
 router.post('/register', ctrl.auth.register);
-router.post('./login', ctrl.auth.login);
+router.post('/login', ctrl.auth.login);
 router.get('/verify', ctrl.auth.verify);
 router.post('/logout', ctrl.auth.logout);
+router.get('/index', ctrl.auth.index);
 
 module.exports = router;
