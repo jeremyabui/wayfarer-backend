c25b41f564af2c9ddf81b884fea9a169d49b7f29
Adonis Martin quiñones <adonisdesuvikdo@gmail.com>
2019-11-21 15:15:19 -0800

    Comments full Crud

--- controllers/comments.js
+++ controllers/comments.js
@@ -15,7 +15,7 @@ const index = (req, res) => {
 };
 
 const show = (req, res) => {
-    db.Comment.findById(req.params.commentId, (err, foundComment) => {
+    db.Comment.findById(req.params.id, (err, foundComment) => {
         if (err) return res.status(500).json(err);
         res.json({
             status: 200,
@@ -25,7 +25,7 @@ const show = (req, res) => {
 };
 
 const create = (req, res) => {
-    db.Comment.create (req.body, (err, createdComment) => {
+    db.Comment.create(req.body, (err, createdComment) => {
         if (err) return console.log(err);
         res.json({
             status: 201,
@@ -33,12 +33,50 @@ const create = (req, res) => {
             data: createdComment,
             requestedAt: new Date().toLocaleString(),
         });
+        console.log(createdComment)
     });
 };
 
+const upDate = (req, res) => {
+    db.Comment.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new: true}, (err, upDatedComment ) => {
+            if (err) return res.status(500).json({
+                status: 500,
+                error:[{message: "something went wrong try again"}]
+            });
+            res.json({
+                status: 200,
+                count: 1,
+                data: upDatedComment,
+                requestedAt: new Date().toLocaleString(),
+              });
+        }
+    );
+}
+
+const destroy = (req, res) => {
+  db.Comment.findByIdAndDelete(
+    req.params.id, ((err, destroyedComment) => {
+      if (err) return res.status(500).json({
+        status: 500,
+        error: [{message: 'Something went wrong! Please try again'}]
+      })
+      res.json({
+        status:200,
+        count: 1,
+        data: destroyedComment,
+        requestedAt: new Date().toLocaleString(),
+      })
+    })      
+  );      
+}
 
 module.exports = {
     index,
     show,
     create,
+    upDate,
+    destroy
 }
