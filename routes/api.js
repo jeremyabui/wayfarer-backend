95355f5890ab47d1c2282e3f422f68133ffa7c8f
jbui <jeremyabui@yahoo.com>
2019-11-21 09:05:59 -0800

    Added cities update and delete route

--- routes/api.js
+++ routes/api.js
@@ -5,5 +5,8 @@ const ctrl = require('../controllers');
 //Post Cre
 router.get('/cities', ctrl.cities.index);
 router.post('/cities/create', ctrl.cities.create);
+router.get('/cities/:cityId', ctrl.cities.show);
+router.put('/cities/update/:cityId', ctrl.cities.update);
+router.delete('/cities/delete/:cityId', ctrl.cities.destroy);
 
 module.exports = router;
