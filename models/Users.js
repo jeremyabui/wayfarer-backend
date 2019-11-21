aeadb9cecf4ad7fc67c12f6e73f9b48834ec984a
jbui <jeremyabui@yahoo.com>
2019-11-20 16:43:16 -0800

    Added auth controllers and updated .env

---
+++ models/Users.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+    },
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+    },
+    currentCity: {
+        type: String,
+        required: [true, 'Current City is required'],
+    },
+    profilePhoto: String,
+    admin: {
+        type: Boolean,
+        default: false,
+    },
+    comment: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
