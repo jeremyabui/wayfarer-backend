2a59fc63b00bbecabaec355000c4dc1a86890adb
jbui <jeremyabui@yahoo.com>
2019-11-22 10:35:33 -0800

    Added multer instance

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
 const cors = require("cors");
+const multer = require('multer');
 const db = require("./models");
 
 require("dotenv").config();
@@ -43,6 +44,29 @@ app.get("/", (req, res) => {
 // Api Route
 app.use("/api/v1", routes.api);
 app.use("/api/v1/auth", routes.auth);
+
+// Multer
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname)
+  }
+});
+
+let upload = multer({ storage: storage }).single('file')
+
+app.post('/upload', function(req, res) {
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      return res.status(500).json(err)
+    } else if (err) {
+      return res.status(500).json(err)
+    }
+    return res.status(200).send(req.file)
+  })
+});
 
 // Server start
 app.listen(PORT, () =>
