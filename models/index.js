6d049960f6dcca4cb2fa5bf20c10bbebf344e244
jbui <jeremyabui@yahoo.com>
2019-11-26 09:33:11 -0800

    Updating for heroku deployment

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
+// mongoose.connect( process.env.MONGODB_URI || "YOUR CURRENT LOCALHOST DB CONNECTION STRING HERE" );
 const dbUrl =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/project-wayfarer";
+mongoose.connect( process.env.MONGODB_URI || "YOUR CURRENT LOCALHOST DB CONNECTION STRING HERE" );
 
 mongoose
   .connect(dbUrl, {
